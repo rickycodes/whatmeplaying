6935230c7692a511de8f6a96e60b35779c638d98
Ricky Miller <ricky.miller@gmail.com>
2017-12-13 08:36:55 -0500

    Interact with bots!

--- index.js
+++ index.js
@@ -3,15 +3,38 @@ const debounce = require('lodash.debounce')
 const config = require('./config')
 const Twit = require('twit')
 const screenshotsPath = '/home/pi/.config/retroarch/screenshots/'
-
-var T = new Twit(config)
+const T = new Twit(config)
 
 const statuses = [
   'I am doing an video game',
   '↑↑↓↓←→←→BA',
   'beep',
-  'boop'
+  'boop',
+  'Hadōken',
+  'PRINCESS IS IN ANOTHER CASTLE!',
+  'FINISH HIM!!'
 ]
+
+const bots = [
+  '@pixelsorter',
+  // '@Lowpolybot',
+  '@ImgShuffleBOT',
+  '@DeepDreamThis',
+  '@a_quilt_bot',
+  '@IMG2ASCII',
+  '@kaleid_o_bot'
+]
+
+const getRandomN = (array, n) => {
+  const shuffled = array.sort(_ => 0.5 - Math.random())
+  return shuffled.slice(0, n)
+}
+
+const getStatus = (a1, a2) => `
+${a1[Math.floor(Math.random() * a1.length)]}
+
+/cc ${getRandomN(a2, 3).join(' ')}
+`
 
 const post = (path, file) => {
   const b64content = fs.readFileSync(`${path}${file}`, { encoding: 'base64' })
@@ -24,7 +47,8 @@ const post = (path, file) => {
 
     T.post('media/metadata/create', metaParams, function (error, data, response) {
       if (!error) {
-        const params = { status: statuses[Math.floor(Math.random() * statuses.length)], media_ids: [mediaIdStr] }
+        const status = getStatus(statuses, bots)
+        const params = { status, media_ids: [mediaIdStr] }
 
         T.post('statuses/update', params, function (error, data, response) {
           if (error) return console.log(error)
