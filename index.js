90de35f5cee6ad7a06570a246de748fb71596796
Ricky Miller <ricky.miller@gmail.com>
2018-01-16 21:20:17 -0500

    :+1:

--- index.js
+++ index.js
@@ -1,48 +1,12 @@
 const fs = require('fs')
 const debounce = require('lodash.debounce')
 const config = require('./config')
-const statuses = require('./src/statuses')
-const staticBots = require('./src/staticBots')
-const getTweetMsg = require('./src/getTweetMsg')
-const screenRecording = require('./src/screenRecording')
+const onRecording = require('./src/onRecording')
+const onScreenshot = require('./src/onScreenshot')
 const Twit = require('twit')
 const screenshotsPath = '/home/pi/.config/retroarch/screenshots/'
 const recordingsPath = '/home/pi/recordings/'
 const T = new Twit(config)
 
-// const gifBots = []
-
-const upload = (file, error, data, response) => {
-  if (error) return console.log(error)
-  const mediaIdStr = data.media_id_string
-  const altText = `what me playing: ${file}`
-  const metaParams = { media_id: mediaIdStr, alt_text: { text: altText } }
-  T.post('media/metadata/create', metaParams, create.bind(null, mediaIdStr))
-}
-
-const create = (mediaIdStr, error, data, response) => {
-  if (error) return console.log(error)
-  const status = getTweetMsg(statuses, staticBots)
-  const params = { status, media_ids: [mediaIdStr] }
-  T.post('statuses/update', params, update)
-}
-
-const update = (error, data, response) => {
-  if (error) return console.log(error)
-  console.log(data)
-}
-
-const postScreenshot = (path, file) => {
-  const b64content = fs.readFileSync(`${path}${file}`, { encoding: 'base64' })
-  T.post('media/upload', { media_data: b64content }, upload.bind(null, file))
-}
-
-const screenshotTaken = (type, file) => {
-  if (file && type === 'change') {
-    console.log('screenshot taken!')
-    postScreenshot(screenshotsPath, file)
-  }
-}
-
-fs.watch(screenshotsPath, debounce(screenshotTaken, 100))
-fs.watch(recordingsPath, screenRecording.bind(null, recordingsPath))
+fs.watch(screenshotsPath, debounce(onScreenshot.bind(null, T, screenshotsPath), 100))
+fs.watch(recordingsPath, onRecording.bind(null, recordingsPath))
---
+++ src/onScreenshot.js
@@ -0,0 +1,38 @@
+const statuses = require('./statuses')
+const staticBots = require('./staticBots')
+const getTweetMsg = require('./getTweetMsg')
+const fs = require('fs')
+
+const onScreenshot = (T, screenshotsPath, type, file) => {
+  const upload = (file, error, data, response) => {
+    if (error) return console.log(error)
+    const mediaIdStr = data.media_id_string
+    const altText = `what me playing: ${file}`
+    const metaParams = { media_id: mediaIdStr, alt_text: { text: altText } }
+    T.post('media/metadata/create', metaParams, create.bind(null, mediaIdStr))
+  }
+
+  const create = (mediaIdStr, error, data, response) => {
+    if (error) return console.log(error)
+    const status = getTweetMsg(statuses, staticBots)
+    const params = { status, media_ids: [mediaIdStr] }
+    T.post('statuses/update', params, update)
+  }
+
+  const update = (error, data, response) => {
+    if (error) return console.log(error)
+    console.log(data)
+  }
+
+  const postScreenshot = (path, file) => {
+    const b64content = fs.readFileSync(`${path}${file}`, { encoding: 'base64' })
+    T.post('media/upload', { media_data: b64content }, upload.bind(null, file))
+  }
+
+  if (file && type === 'change') {
+    console.log('screenshot taken!')
+    postScreenshot(screenshotsPath, file)
+  }
+}
+
+module.exports = onScreenshot
